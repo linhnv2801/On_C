2533b29369147b252647828addba78fe2e00d9f4
Nguyen Van Linh <linh.nv@samsung.com>
2017-06-28 08:41:14 +0700

    stl stack emplace

--- stl_stack.cpp
+++ stl_stack.cpp
@@ -1,25 +1,21 @@
-// constructing stacks
-#include <iostream>       // std::cout
+// stack::emplace
+#include <iostream>       // std::cin, std::cout
 #include <stack>          // std::stack
-#include <vector>         // std::vector
-#include <deque>          // std::deque
+#include <string>         // std::string, std::getline(string)
 
 int main ()
 {
-  std::deque<int> mydeque (3,100);          // deque with 3 elements
-  std::vector<int> myvector (2,200);        // vector with 2 elements
+  std::stack<std::string> mystack;
 
-  std::stack<int> first;                    // empty stack
-  std::stack<int> second (mydeque);         // stack initialized to copy of deque
+  mystack.emplace ("First sentence");
+  mystack.emplace ("Second sentence");
 
-  std::stack<int,std::vector<int> > third;  // empty stack using vector
-  std::stack<int,std::vector<int> > fourth (myvector);
-
-  std::cout << "size of first: " << first.size() << '\n';
-  std::cout << "size of second: " << second.size() << '\n';
-  std::cout << "size of third: " << third.size() << '\n';
-  std::cout << "size of fourth: " << fourth.size() << '\n';
+  std::cout << "mystack contains:\n";
+  while (!mystack.empty())
+  {
+    std::cout << mystack.top() << '\n';
+    mystack.pop();
+  }
 
   return 0;
 }
-
