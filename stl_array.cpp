212bbcc152a5e4c155009dac405a76b92fcd8d3a
Nguyen Van Linh <linh.nv@samsung.com>
2017-06-28 09:28:03 +0700

    stl array back & front

--- stl_array.cpp
+++ stl_array.cpp
@@ -1,24 +1,18 @@
-// constructing arrays
+// array::back
 #include <iostream>
 #include <array>
 
-// default initialization (non-local = static storage):
-std::array<int,3> global;               // zero-initialized: {0,0,0}
-
 int main ()
 {
-  // default initialization (local = automatic storage):
-  std::array<int,3> first;              // uninitialized:    {?,?,?}
+  std::array<int,3> myarray = {5, 19, 77};
 
-  // initializer-list initializations:
-  std::array<int,3> second = {10,20};   // initialized as:   {10,20,0}
-  std::array<int,3> third = {1,2,3};    // initialized as:   {1,2,3}
+  std::cout << "front is: " << myarray.front() << std::endl;   // 5
+  std::cout << "back is: " << myarray.back() << std::endl;     // 77
 
-  // copy initialization:
-  std::array<int,3> fourth = third;     // copy:             {1,2,3}
+  myarray.back() = 50;
 
-  std::cout << "The contents of fourth are:";
-  for (auto x:fourth) std::cout << ' ' << x;
+  std::cout << "myarray now contains:";
+  for ( int& x : myarray ) std::cout << ' ' << x;
   std::cout << '\n';
 
   return 0;
