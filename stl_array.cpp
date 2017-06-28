799bad0e5473450f845b60384bff33bc78d77a06
Nguyen Van Linh <linh.nv@samsung.com>
2017-06-28 10:45:03 +0700

    stl array get array as tuples

--- stl_array.cpp
+++ stl_array.cpp
@@ -1,21 +1,21 @@
-// swap arrays
+// arrays as tuples
 #include <iostream>
 #include <array>
+#include <tuple>
 
 int main ()
 {
-  std::array<int,5> first = {10, 20, 30, 40, 50};
-  std::array<int,5> second = {11, 22, 33, 44, 55};
+  std::array<int,3> myarray = {10, 20, 30};
+  std::tuple<int,int,int> mytuple (10, 20, 30);
 
-  first.swap (second);
+  std::tuple_element<0,decltype(myarray)>::type myelement;  // int myelement
 
-  std::cout << "first:";
-  for (int& x : first) std::cout << ' ' << x;
-  std::cout << '\n';
+  myelement = std::get<2>(myarray);
+  std::get<2>(myarray) = std::get<0>(myarray);
+  std::get<0>(myarray) = myelement;
 
-  std::cout << "second:";
-  for (int& x : second) std::cout << ' ' << x;
-  std::cout << '\n';
+  std::cout << "first element in myarray: " << std::get<0>(myarray) << "\n";
+  std::cout << "first element in mytuple: " << std::get<0>(mytuple) << "\n";
 
   return 0;
 }
