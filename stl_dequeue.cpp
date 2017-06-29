08cfa76f742f15dcb14b3ff783287c58a163670f
Nguyen Van Linh <linh.nv@samsung.com>
2017-06-29 10:22:12 +0700

    assignment operator with deques

--- stl_dequeue.cpp
+++ stl_dequeue.cpp
@@ -1,26 +1,16 @@
-// constructing deques
+// assignment operator with deques
 #include <iostream>
 #include <deque>
 
 int main ()
 {
-  unsigned int i;
+  std::deque<int> first (3);    // deque with 3 zero-initialized ints
+  std::deque<int> second (5);   // deque with 5 zero-initialized ints
 
-  // constructors used in the same order as described above:
-  std::deque<int> first;                                // empty deque of ints
-  std::deque<int> second (4,100);                       // four ints with value 100
-  std::deque<int> third (second.begin(),second.end());  // iterating through second
-  std::deque<int> fourth (third);                       // a copy of third
+  second = first;
+  first = std::deque<int>();
 
-  // the iterator constructor can be used to copy arrays:
-  int myints[] = {16,2,77,29};
-  std::deque<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
-
-  std::cout << "The contents of fifth are:";
-  for (std::deque<int>::iterator it = fifth.begin(); it!=fifth.end(); ++it)
-    std::cout << ' ' << *it;
-
-  std::cout << '\n';
-
+  std::cout << "Size of first: " << int (first.size()) << '\n';
+  std::cout << "Size of second: " << int (second.size()) << '\n';
   return 0;
 }
