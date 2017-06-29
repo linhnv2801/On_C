564bee7c099371bacff2c5293eb5ab009c54ebbc
Nguyen Van Linh <linh.nv@samsung.com>
2017-06-29 16:46:11 +0700

    const member function 2

--- const_member_function.cpp
+++ const_member_function.cpp
@@ -9,10 +9,16 @@ public:
     // We get compiler error if we add a line like "value = 100;"
     // in this function.
     int getValue() const {return value;}  
+    int* const get() {
+    	return &value;
+	}
 };
  
 int main() {
     Test t(20);
-    cout<<t.getValue();
+    cout<<t.getValue() << endl;
+    *t.get() = 4;
+    cout<<t.getValue() << endl;
+    
     return 0;
 }
