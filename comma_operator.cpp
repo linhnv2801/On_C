49035fc64314850ddfa14b8a1be897f83e131a07
Nguyen Van Linh <linh.nv@samsung.com>
2017-06-30 10:54:06 +0700

    comma operator

---
+++ comma_operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    /**
+	*  Commas act as separators in this line, not as an operator.
+	*  Results: a=1, b=2, c=3, i=0
+	*/
+    int a = 1, b = 2, c = 3, i = 0;
+}
+
