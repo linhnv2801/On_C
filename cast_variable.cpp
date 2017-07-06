592258d5659a590c180d35cfdef7f2d772131b10
Nguyen Van Linh <linh.nv@samsung.com>
2017-07-06 10:38:07 +0700

    const cast exercise 1 geekforgeek

--- cast_variable.cpp
+++ cast_variable.cpp
@@ -5,9 +5,15 @@ using namespace std;
 int main(void)
 {
     int a1 = 40;
-    const volatile int* b1 = &a1;
-    cout << "typeid of b1 " << typeid(b1).name() << '\n';
-    int* c1 = const_cast <int *> (b1);
-    cout << "typeid of c1 " << typeid(c1).name() << '\n';
+    const int* b1 = &a1;
+    char* c1 = (char *)(b1);
+    cout << "Type of b1 " << typeid(b1).name() << endl;
+//    char *c = const_cast <char *> (b1); // compile error, because b1 & c not same type
+    *c1 = 'A';
+    a1 = 50;
+    *c1 = 'B';
+    cout << "Type of b1 " << typeid(b1).name() << endl;
+    cout << *b1 << endl;
+    cout << *c1 << endl;
     return 0;
 }
