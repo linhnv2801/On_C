5f8c7a8a6fdd2680125394b1ddf0d2f9bd5dc89b
Nguyen Van Linh <linh.nv@samsung.com>
2017-07-07 11:05:33 +0700

    static cast

--- cast_variable.cpp
+++ cast_variable.cpp
@@ -1,19 +1,70 @@
+#include <vector>
 #include <iostream>
-#include <typeinfo>
-using namespace std;
+
+using namespace std; 
+
+struct B {
+    int m = 0;
+    void hello() const {
+        std::cout << "Hello world, this is B!\n";
+    }
+};
+struct D : B {
+    void hello() const {
+        std::cout << "Hello world, this is D!\n";
+    }
+};
  
-int main(void)
+enum class E { ONE = 1, TWO, THREE };
+enum EU { ONE = 2, TWO, THREE };
+ 
+int main()
 {
-    int a1 = 40;
-    const int* b1 = &a1;
-    char* c1 = (char *)(b1);
-    cout << "Type of b1 " << typeid(b1).name() << endl;
-//    char *c = const_cast <char *> (b1); // compile error, because b1 & c not same type
-    *c1 = 'A';
-    a1 = 50;
-    *c1 = 'B';
-    cout << "Type of b1 " << typeid(b1).name() << endl;
-    cout << *b1 << endl;
-    cout << *c1 << endl;
-    return 0;
+    // 1: initializing conversion
+    int n = static_cast<int>(3.14); 
+    std::cout << "n = " << n << '\n';
+    std::vector<int> v = static_cast<std::vector<int>>(10);
+    std::cout << "v.size() = " << v.size() << '\n';
+ 
+    // 2: static downcast
+    D d;
+    B& br = d; // upcast via implicit conversion
+    br.hello();
+    D& another_d = static_cast<D&>(br); // downcast
+    another_d.hello();
+ 
+    // 3: lvalue to xvalue
+    std::vector<int> v2 = static_cast<std::vector<int>&&>(v);
+    std::cout << "after move, v.size() = " << v.size() << '\n';
+ 
+    // 4: discarded-value expression
+    static_cast<void>(v2.size());
+ 
+    // 5. inverse of implicit conversion
+    void* nv = &n;
+    int* ni = static_cast<int*>(nv);
+    std::cout << "*ni = " << *ni << '\n';
+ 
+    // 6. array-to-pointer followed by upcast
+    D a[10];
+    B* dp = static_cast<B*>(a);
+    a[0].hello();
+    (dp+2)->hello();
+ 
+    // 7. scoped enum to int or float
+    E e = E::ONE;
+    int one = static_cast<int>(e);
+    std::cout << one << '\n';
+ 
+    // 8. int to enum, enum to another enum
+    E e2 = static_cast<E>(one);
+    EU eu = static_cast<EU>(e2);
+ 
+    // 9. pointer to member upcast
+    int D::*pm = &D::m;
+    std::cout << br.*static_cast<int B::*>(pm) << '\n';
+ 
+    // 10. void* to any type
+    void* voidp = &e;
+    std::vector<int>* p = static_cast<std::vector<int>*>(voidp);
 }
