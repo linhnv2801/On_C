6ea5c6a3d275da42267a8f78672476e9bf4f07c5
Nguyen Van Linh <linh.nv@samsung.com>
2017-07-17 13:36:55 +0700

    algorithm copy_backward

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,19 +1,36 @@
-// copy algorithm example
-#include <iostream>     // std::cout
-#include <algorithm>    // std::copy
-#include <vector>       // std::vector
+// copy_backward example
+#include <iostream> // std::cout
+#include <algorithm> // std::copy_backward
+#include <vector> // std::vector
 
-int main () {
-  int myints[]={10,20,30,40,50,60,70};
-  std::vector<int> myvector (7);
+//template <class T>
+void print(float i){
+	std::cout << i << "\t";
+}
 
-  std::copy ( myints, myints+6, myvector.begin());
+int main()
+{
+    std::vector<float> myvector;
 
-  std::cout << "myvector contains:";
-  for (std::vector<int>::iterator it = myvector.begin(); it!=myvector.end(); ++it)
-    std::cout << ' ' << *it;
+    // set some values:
+    for (float i = 1; i <= 5; i++)
+        myvector.push_back(i * 10); // myvector: 10 20 30 40 50
+    
+    std::cout << myvector.size() << "\n";
+    for_each(myvector.begin(),myvector.end(),print);
+    std::cout << "\n";
+    
+    std::cout << *(myvector.begin()) << "\n";
+    std::cout << *(--std::end(myvector)) << "\n";
 
-  std::cout << '\n';
+    myvector.resize(myvector.size() + 3); // allocate space for 3 more elements
 
-  return 0;
+    std::copy_backward(myvector.begin(), myvector.begin() + 8, myvector.end());
+
+    std::cout << "myvector contains:";
+    for (std::vector<float>::iterator it = myvector.begin(); it != myvector.end(); ++it)
+        std::cout << ' ' << *it;
+    std::cout << '\n';
+
+    return 0;
 }
