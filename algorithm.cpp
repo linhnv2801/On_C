43a245d8432a3366be496bb0663be38c9aba3b7e
Nguyen Van Linh <linh.nv@samsung.com>
2017-07-18 14:35:14 +0700

    move_backward algorithm

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,36 +1,20 @@
-// move algorithm example
+// move_backward example
 #include <iostream>     // std::cout
-#include <algorithm>    // std::move (ranges)
-#include <utility>      // std::move (objects)
-#include <vector>       // std::vector
+#include <algorithm>    // std::move_backward
 #include <string>       // std::string
 
 int main () {
-  std::vector<std::string> foo = {"air","water","fire","earth"};
-  std::vector<std::string> bar (4);
+	std::string elems[10] = {"air","water","fire","earth"};
 
-  // moving ranges:
-  std::cout << "Moving ranges...\n";
-  std::move ( foo.begin(), foo.begin()+4, bar.begin() );
+	std::cout << *(elems+3) << "\n";
+	// insert new element at the beginning:
+	std::move_backward (elems,elems+4,elems+5);
+	//elems[0]="ether";
 
-  std::cout << "foo contains " << foo.size() << " elements:";
-  std::cout << " (each in an unspecified but valid state)";
-  std::cout << '\n';
+	std::cout << "elems contains:";
+	for (int i=0; i<10; ++i)
+		std::cout << " [" << elems[i] << "]";
+	std::cout << '\n';
 
-  std::cout << "bar contains " << bar.size() << " elements:";
-  for (std::string& x: bar) std::cout << " [" << x << "]";
-  std::cout << '\n';
-
-  // moving container:
-  std::cout << "Moving container...\n";
-  foo = std::move (bar);
-
-  std::cout << "foo contains " << foo.size() << " elements:";
-  for (std::string& x: foo) std::cout << " [" << x << "]";
-  std::cout << '\n';
-
-  std::cout << "bar is in an unspecified but valid state";
-  std::cout << '\n';
-
-  return 0;
+	return 0;
 }
