d8305e75c02a8d9cd3a158ece1f34f61fff475e1
Nguyen Van Linh <linh.nv@samsung.com>
2017-07-19 13:50:13 +0700

    algorithm rotate

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,19 +1,20 @@
-// remove algorithm example
+// rotate algorithm example
 #include <iostream>     // std::cout
-#include <algorithm>    // std::remove
+#include <algorithm>    // std::rotate
+#include <vector>       // std::vector
 
 int main () {
-  int myints[] = {10,20,30,30,20,10,10,20};      // 10 20 30 30 20 10 10 20
+  std::vector<int> myvector;
 
-  // bounds of range:
-  int* pbegin = myints;                          // ^
-  int* pend = myints+sizeof(myints)/sizeof(int); // ^                       ^
+  // set some values:
+  for (int i=1; i<10; ++i) myvector.push_back(i); // 1 2 3 4 5 6 7 8 9
 
-  pend = std::remove (pbegin, pend, 20);         // 10 30 30 10 10 ?  ?  ?
-                                                 // ^              ^
-  std::cout << "range contains:";
-  for (int* p=pbegin; p!=pend; ++p)
-    std::cout << ' ' << *p;
+  std::rotate(myvector.begin(),myvector.begin()+3,myvector.end());
+                                                  // 4 5 6 7 8 9 1 2 3
+  // print out content:
+  std::cout << "myvector contains:";
+  for (std::vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it)
+    std::cout << ' ' << *it;
   std::cout << '\n';
 
   return 0;
