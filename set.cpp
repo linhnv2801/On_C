adabd38ad9249669291894dd545d3f6cafb07428
Nguyen Van Linh <linh.nv@samsung.com>
2017-08-04 15:37:45 +0700

    set count

--- set.cpp
+++ set.cpp
@@ -1,23 +1,22 @@
-// set::find
+// set::count
 #include <iostream>
 #include <set>
 
 int main ()
 {
   std::set<int> myset;
-  std::set<int>::iterator it;
 
   // set some initial values:
-  for (int i=1; i<=5; i++) myset.insert(i*10);    // set: 10 20 30 40 50
+  for (int i=1; i<5; ++i) myset.insert(i*3);    // set: 3 6 9 12
 
-  it=myset.find(20);
-  myset.erase (it);
-  myset.erase (myset.find(40));
-
-  std::cout << "myset contains:";
-  for (it=myset.begin(); it!=myset.end(); ++it)
-    std::cout << ' ' << *it;
-  std::cout << '\n';
+  for (int i=0; i<10; ++i)
+  {
+    std::cout << i;
+    if (myset.count(i)!=0)
+      std::cout << " is an element of myset.\n";
+    else
+      std::cout << " is not an element of myset.\n";
+  }
 
   return 0;
 }
