17c50cdaf422f5d7c6008f3a866cfb967fc742f8
Nguyen Van Linh <linh.nv@samsung.com>
2017-08-08 14:45:54 +0700

    random_shuffle

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,31 +1,45 @@
-// rotate_copy algorithm example
+// random_shuffle example
 #include <iostream>     // std::cout
-#include <algorithm>    // std::rotate_copy
+#include <algorithm>    // std::random_shuffle
 #include <vector>       // std::vector
+#include <ctime>        // std::time
+#include <cstdlib>      // std::rand, std::srand
 
 namespace test{
-	template <class ForwardIterator, class OutputIterator>
-	OutputIterator rotate_copy (ForwardIterator first, ForwardIterator middle,
-		ForwardIterator last, OutputIterator result)
+	template <class RandomAccessIterator, class RandomNumberGenerator>
+	  void random_shuffle (RandomAccessIterator first, RandomAccessIterator last,
+	                       RandomNumberGenerator& gen)
 	{
-		result=std::copy (middle,last,result);
-		return std::copy (first,middle,result);
+	  typename std::iterator_traits<RandomAccessIterator>::difference_type i, n;
+	  n = (last-first);
+	  for (i=n-1; i>0; --i) {
+	    std::swap (first[i],first[gen(i+1)]);
+	  }
 	}
 }
 
+// random generator function:
+int myrandom (int i) { return std::rand()%i;}
+
 int main () {
-	int myints[] = {10,20,30,40,50,60,70};
+  std::srand ( unsigned ( std::time(0) ) );
+  std::vector<int> myvector;
 
-	std::vector<int> myvector (7);
+  // set some values:
+  for (int i=1; i<10; ++i) myvector.push_back(i); // 1 2 3 4 5 6 7 8 9
 
-	test::rotate_copy(myints,myints+3,myints+7,myvector.begin());
+  // using built-in random generator:
+  std::random_shuffle ( myvector.begin(), myvector.end() );
 
-	// print out content:
-	std::cout << "myvector contains:";
-	for (std::vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it)
-		std::cout << ' ' << *it;
-	std::cout << '\n';
+  // using myrandom:
+  test::random_shuffle ( myvector.begin(), myvector.end(), myrandom);
 
-	return 0;
+  // print out content:
+  std::cout << "myvector contains:";
+  for (std::vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it)
+    std::cout << ' ' << *it;
+
+  std::cout << '\n';
+
+  return 0;
 }
-
