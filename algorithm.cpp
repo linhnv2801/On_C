7f7287710f54bad366ce760d676e33df227f6523
Nguyen Van Linh <linh.nv@samsung.com>
2017-08-08 14:41:43 +0700

    rotate_copy

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,21 +1,31 @@
-// rotate algorithm example
+// rotate_copy algorithm example
 #include <iostream>     // std::cout
-#include <algorithm>    // std::rotate
+#include <algorithm>    // std::rotate_copy
 #include <vector>       // std::vector
 
-int main () {
-  std::vector<int> myvector;
-
-  // set some values:
-  for (int i=1; i<10; ++i) myvector.push_back(i); // 1 2 3 4 5 6 7 8 9
-
-  std::rotate(myvector.begin(),myvector.begin()+3,myvector.end());
-                                                  // 4 5 6 7 8 9 1 2 3
-  // print out content:
-  std::cout << "myvector contains:";
-  for (std::vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it)
-    std::cout << ' ' << *it;
-  std::cout << '\n';
-
-  return 0;
+namespace test{
+	template <class ForwardIterator, class OutputIterator>
+	OutputIterator rotate_copy (ForwardIterator first, ForwardIterator middle,
+		ForwardIterator last, OutputIterator result)
+	{
+		result=std::copy (middle,last,result);
+		return std::copy (first,middle,result);
+	}
 }
+
+int main () {
+	int myints[] = {10,20,30,40,50,60,70};
+
+	std::vector<int> myvector (7);
+
+	test::rotate_copy(myints,myints+3,myints+7,myvector.begin());
+
+	// print out content:
+	std::cout << "myvector contains:";
+	for (std::vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it)
+		std::cout << ' ' << *it;
+	std::cout << '\n';
+
+	return 0;
+}
+
