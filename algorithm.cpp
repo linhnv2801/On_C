ec143bdf016a8559d0f653610e23c8b4f038b18a
Nguyen Van Linh <linh.nv@samsung.com>
2017-08-08 15:42:54 +0700

    stable_partition

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,28 +1,7 @@
-// partition algorithm example
+// stable_partition example
 #include <iostream>     // std::cout
-#include <algorithm>    // std::partition
+#include <algorithm>    // std::stable_partition
 #include <vector>       // std::vector
-
-namespace algorithm {
-	template <class BidirectionalIterator, class UnaryPredicate>
-	  BidirectionalIterator partition (BidirectionalIterator first,
-	                                   BidirectionalIterator last, UnaryPredicate pred)
-	{
-	  while (first!=last) {
-	    while (pred(*first)) {
-	      ++first;
-	      if (first==last) return first;
-	    }
-	    do {
-	      --last;
-	      if (first==last) return first;
-	    } while (!pred(*last));
-	    swap (*first,*last);
-	    ++first;
-	  }
-	  return first;
-	}
-}
 
 bool IsOdd (int i) { return (i%2)==1; }
 
@@ -33,7 +12,7 @@ int main () {
   for (int i=1; i<10; ++i) myvector.push_back(i); // 1 2 3 4 5 6 7 8 9
 
   std::vector<int>::iterator bound;
-  bound = std::partition (myvector.begin(), myvector.end(), IsOdd);
+  bound = std::stable_partition (myvector.begin(), myvector.end(), IsOdd);
 
   // print out content:
   std::cout << "odd elements:";
