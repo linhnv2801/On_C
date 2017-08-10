1504219b9d6808b58478c685d23fa8f4408a28df
Nguyen Van Linh <linh.nv@samsung.com>
2017-08-10 11:08:06 +0700

    partition_point

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,47 +1,41 @@
-// partition_copy example
+// partition_point example
 #include <iostream>     // std::cout
-#include <algorithm>    // std::partition_copy, std::count_if
+#include <algorithm>    // std::partition, std::partition_point
 #include <vector>       // std::vector
 
-namespace algorithm {
-	template <class InputIterator, class OutputIterator1,
-	class OutputIterator2, class UnaryPredicate>
-		std::pair<OutputIterator1,OutputIterator2>
-		partition_copy (InputIterator first, InputIterator last,
-		OutputIterator1 result_true, OutputIterator2 result_false,
-		UnaryPredicate pred)
+namespace algorithm{
+	template <class ForwardIterator, class UnaryPredicate>
+	  ForwardIterator partition_point (ForwardIterator first, ForwardIterator last,
+	                                   UnaryPredicate pred)
 	{
-		while (first!=last) {
-			if (pred(*first)) {
-				*result_true = *first;
-				++result_true;
-			}
-			else {
-				*result_false = *first;
-				++result_false;
-			}
-			++first;
-		}
-		return std::make_pair (result_true,result_false);
+	  auto n = distance(first,last);
+	  while (n>0)
+	  {
+	    ForwardIterator it = first;
+	    auto step = n/2;
+	    std::advance (it,step);
+	    if (pred(*it)) { first=++it; n-=step+1; }
+	    else n=step;
+	  }
+	  return first;
 	}
 }
 
 bool IsOdd (int i) { return (i%2)==1; }
 
 int main () {
-	std::vector<int> foo {1,2,3,4,5,6,7,8,9};
-	std::vector<int> odd, even;
+  std::vector<int> foo {1,2,3,4,5,6,7,8,9};
+  std::vector<int> odd;
 
-	// resize vectors to proper size:
-	unsigned n = std::count_if (foo.begin(), foo.end(), IsOdd);
-	odd.resize(n); even.resize(foo.size()-n);
+  std::partition (foo.begin(),foo.end(),IsOdd);
 
-	// partition:
-	std::partition_copy (foo.begin(), foo.end(), odd.begin(), even.begin(), IsOdd);
+  auto it = std::partition_point(foo.begin(),foo.end(),IsOdd);
+  odd.assign (foo.begin(),it);
 
-	// print contents:
-	std::cout << "odd: ";  for (int& x:odd)  std::cout << ' ' << x; std::cout << '\n';
-	std::cout << "even: "; for (int& x:even) std::cout << ' ' << x; std::cout << '\n';
+  // print contents of odd:
+  std::cout << "odd:";
+  for (int& x:odd) std::cout << ' ' << x;
+  std::cout << '\n';
 
-	return 0;
+  return 0;
 }
