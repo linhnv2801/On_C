0d3aed6cdaaf608814c3365cc0df87db80bb067b
Nguyen Van Linh <linh.nv@samsung.com>
2017-08-15 09:57:56 +0700

    set_union

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,37 +1,44 @@
-// includes algorithm example
+// set_union example
 #include <iostream>     // std::cout
-#include <algorithm>    // std::includes, std::sort
+#include <algorithm>    // std::set_union, std::sort
+#include <vector>       // std::vector
 
-namespace algorithm {
-	template <class InputIterator1, class InputIterator2>
-	  bool includes (InputIterator1 first1, InputIterator1 last1,
-	                 InputIterator2 first2, InputIterator2 last2)
+namespace algorithm{
+	template <class InputIterator1, class InputIterator2, class OutputIterator>
+	  OutputIterator set_union (InputIterator1 first1, InputIterator1 last1,
+	                            InputIterator2 first2, InputIterator2 last2,
+	                            OutputIterator result)
 	{
-	  while (first2!=last2) {
-	    if ( (first1==last1) || (*first2<*first1) ) return false;
-	    if (!(*first1<*first2)) ++first2;
-	    ++first1;
+	  while (true)
+	  {
+	    if (first1==last1) return std::copy(first2,last2,result);
+	    if (first2==last2) return std::copy(first1,last1,result);
+	
+	    if (*first1<*first2) { *result = *first1; ++first1; }
+	    else if (*first2<*first1) { *result = *first2; ++first2; }
+	    else { *result = *first1; ++first1; ++first2; }
+	    ++result;
 	  }
-	  return true;
 	}
 }
 
-bool myfunction (int i, int j) { return i>j; }
-
 int main () {
-  int container[] = {5,10,15,20,25,30,35,40,45,50};
-  int continent[] = {40,30,25,10};
+  int first[] = {5,10,15,20,25,25,40,40,45,65,75};
+  int second[] = {5,5,50,40,30,20,25,25,25,10};
+  std::vector<int> v(sizeof(first)/sizeof(int) + sizeof(second)/sizeof(int));                      // 0  0  0  0  0  0  0  0  0  0
+  std::vector<int>::iterator it;
 
-  std::sort (container,container+10, myfunction);
-  std::sort (continent,continent+4, myfunction);
+  std::sort (first, first + sizeof(first)/sizeof(int));     //  5 10 15 20 25
+  std::sort (second,second+ sizeof(second)/sizeof(int));   // 10 20 30 40 50
 
-  // using default comparison:
-  if ( std::includes(container,container+10,continent,continent+4) )
-    std::cout << "container includes continent!\n";
+  it=std::set_union (first, first+ sizeof(first)/sizeof(int), second, second+sizeof(second)/sizeof(int), v.begin());
+                                               // 5 10 15 20 25 30 40 50  0  0
+  v.resize(it-v.begin());                      // 5 10 15 20 25 30 40 50
 
-  // using myfunction as comp:
-  if ( std::includes(container,container+10,continent,continent+4, myfunction) )
-    std::cout << "container includes continent!\n";
+  std::cout << "The union has " << (v.size()) << " elements:\n";
+  for (it=v.begin(); it!=v.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
 
   return 0;
 }
