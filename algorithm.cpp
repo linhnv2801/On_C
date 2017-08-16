2626347aaa8c4e1bed34f3fe4165ebf13979c6a4
Nguyen Van Linh <linh.nv@samsung.com>
2017-08-16 10:16:27 +0700

    push_heap

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,44 +1,40 @@
-// set_symmetric_difference example
+// range heap example
 #include <iostream>     // std::cout
-#include <algorithm>    // std::set_symmetric_difference, std::sort
+#include <algorithm>    // std::make_heap, std::pop_heap, std::push_heap, std::sort_heap
 #include <vector>       // std::vector
 
-namespace algorithm{
-	template <class InputIterator1, class InputIterator2, class OutputIterator>
-	  OutputIterator set_symmetric_difference (InputIterator1 first1, InputIterator1 last1,
-	                                           InputIterator2 first2, InputIterator2 last2,
-	                                           OutputIterator result)
-	{
-	  while (true)
-	  {
-	    if (first1==last1) return std::copy(first2,last2,result);
-	    if (first2==last2) return std::copy(first1,last1,result);
-	
-	    if (*first1<*first2) { *result=*first1; ++result; ++first1; }
-	    else if (*first2<*first1) { *result = *first2; ++result; ++first2; }
-	    else { ++first1; ++first2; }
-	  }
+class comp{
+	bool reverse;
+public:
+	comp(bool param){
+		reverse = param;
+	}	
+	bool operator()(const int& lhs, const int& rhs) const{
+		if(reverse) return (lhs > rhs);
+		else return (lhs < rhs);
 	}
-}
+};
 
 int main () {
-  int first[] = {5,10,15,20,25};
-  int second[] = {50,40,30,20,10};
-  std::vector<int> v(10);                      // 0  0  0  0  0  0  0  0  0  0
-  std::vector<int>::iterator it;
+  int myints[] = {10,20,30,5,15,100};
+  std::vector<int> v(myints,myints+sizeof(myints)/sizeof(int));
 
-  std::sort (first,first+5);     //  5 10 15 20 25
-  std::sort (second,second+5);   // 10 20 30 40 50
+  std::make_heap (v.begin(),v.end(),comp(true));
+  std::cout << "initial max heap   : " << v.front() <<"\t"<< v.back()<< '\n';
 
-  it=std::set_symmetric_difference (first, first+5, second, second+5, v.begin());
-                                               //  5 15 25 30 40 50  0  0  0  0
-  v.resize(it-v.begin());                      //  5 15 25 30 40 50
+  std::pop_heap (v.begin(),v.end()); v.pop_back();
+  std::cout << "max heap after pop : " << v.front() << '\n';
 
-  std::cout << "The symmetric difference has " << (v.size()) << " elements:\n";
-  for (it=v.begin(); it!=v.end(); ++it)
-    std::cout << ' ' << *it;
+  v.push_back(99); std::push_heap (v.begin(),v.end());
+  std::cout << "max heap after push: " << v.front() << '\n';
+
+  std::sort_heap (v.begin(),v.end());
+
+  std::cout << "final sorted range :";
+  for (unsigned i=0; i<v.size(); i++)
+    std::cout << ' ' << v[i];
+
   std::cout << '\n';
 
   return 0;
 }
-
